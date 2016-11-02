6bb363b34482572fe68b0c1d9bbcd59f4fd1ae9a
philipycha <philip.yc.ha@gmail.com>
2016-11-02 13:01:44 -0700

    added animation to assets

--- Overrun/ViewController.swift
+++ Overrun/ViewController.swift
@@ -13,6 +13,8 @@ import SceneKit
 
 class ViewController: UIViewController, GMSMapViewDelegate, LocationManagerDelegate, SignInDelegate, RunManagerDelegate, CAAnimationDelegate {
     
+    @IBOutlet weak var middleArchStandby: UIView!
+    @IBOutlet weak var bigArchStandby: UIView!
     @IBOutlet weak var PlayerAnimationView: UIView!
     @IBOutlet weak var distanceLabel: UILabel!
     @IBOutlet weak var distanceView: UIView!
@@ -41,51 +43,51 @@ class ViewController: UIViewController, GMSMapViewDelegate, LocationManagerDeleg
         runManager.delegate = self
     
         
-        // create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // create and add a camera to the scene
-        let cameraNode = SCNNode()
-        cameraNode.camera = SCNCamera()
-        scene.rootNode.addChildNode(cameraNode)
-        
-        // place the camera
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
-        
-        // create and add a light to the scene
-        let lightNode = SCNNode()
-        lightNode.light = SCNLight()
-        lightNode.light!.type = .omni
-        lightNode.position = SCNVector3(x: 0, y: 5, z: 10)
-        scene.rootNode.addChildNode(lightNode)
-        
-        // create and add an ambient light to the scene
-        let ambientLightNode = SCNNode()
-        ambientLightNode.light = SCNLight()
-        ambientLightNode.light!.type = .ambient
-        ambientLightNode.light!.color = UIColor.darkGray
-        scene.rootNode.addChildNode(ambientLightNode)
-        
-        // retrieve the ship node
-        let ship = scene.rootNode.childNode(withName: "ship", recursively: true)!
-        
-        // animate the 3d object
-        ship.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 1, y: 1, z: 0, duration: 1)))
-        
-        // retrieve the SCNView
-        let scnView = PlayerAnimationView as! SCNView
-        
-        // set the scene to the view
-        scnView.scene = scene
-        
-        // allows the user to manipulate the camera
-        scnView.allowsCameraControl = false
-        
-        // show statistics such as fps and timing information
-        scnView.showsStatistics = false
-        
-        // configure the view
-        scnView.backgroundColor = UIColor.clear
+//        // create a new scene
+//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+//        
+//        // create and add a camera to the scene
+//        let cameraNode = SCNNode()
+//        cameraNode.camera = SCNCamera()
+//        scene.rootNode.addChildNode(cameraNode)
+//        
+//        // place the camera
+//        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
+//        
+//        // create and add a light to the scene
+//        let lightNode = SCNNode()
+//        lightNode.light = SCNLight()
+//        lightNode.light!.type = .omni
+//        lightNode.position = SCNVector3(x: 0, y: 5, z: 10)
+//        scene.rootNode.addChildNode(lightNode)
+//        
+//        // create and add an ambient light to the scene
+//        let ambientLightNode = SCNNode()
+//        ambientLightNode.light = SCNLight()
+//        ambientLightNode.light!.type = .ambient
+//        ambientLightNode.light!.color = UIColor.darkGray
+//        scene.rootNode.addChildNode(ambientLightNode)
+//        
+//        // retrieve the ship node
+//        let ship = scene.rootNode.childNode(withName: "ship", recursively: true)!
+//        
+//        // animate the 3d object
+//        ship.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 1, y: 1, z: 0, duration: 1)))
+//        
+//        // retrieve the SCNView
+//        let scnView = PlayerAnimationView as! SCNView
+//        
+//        // set the scene to the view
+//        scnView.scene = scene
+//        
+//        // allows the user to manipulate the camera
+//        scnView.allowsCameraControl = false
+//        
+//        // show statistics such as fps and timing information
+//        scnView.showsStatistics = false
+//        
+//        // configure the view
+//        scnView.backgroundColor = UIColor.clear
         
         mapView = GMSMapView()
 
@@ -127,8 +129,11 @@ class ViewController: UIViewController, GMSMapViewDelegate, LocationManagerDeleg
     
     override func viewDidAppear(_ animated: Bool) {
         
-        animate.rotateClockwise(view: startRunButtonView)
-        animate.rotateCounterClockwise(view: endRunButtonView)
+        animate.rotateSlowClockwise(view: startRunButtonView)
+        animate.rotateCounterClockwise(view: middleArchStandby)
+        animate.rotateClockwise(view: bigArchStandby)
+        
+//        animate.rotateCounterClockwise(view: endRunButtonView)
         
         
     }
